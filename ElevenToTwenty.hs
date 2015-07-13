1c1531e35af7f055b83c667d70ef02f1f29f2f02
nickmyers217 <nickbmyers217@gmail.com>
2015-07-13 15:20:51 -0500

    Should be working now

--- ElevenToTwenty.hs
+++ ElevenToTwenty.hs
@@ -4,20 +4,16 @@ import OneToTen(encode)
 
 -- | Problem 11
 data Encode a = Multiple Int a
-			  | Single a deriving (Show)
+              | Single a deriving (Show)
 
 
 encodeModified :: Eq a => [a] -> [Encode a]
 encodeModified = map go . encode
-	where go (c,a) = if c == 1 then Single a else Multiple c a
+    where go (c,a) = if c == 1 then Single a else Multiple c a
+
 
 -- | Problem 12
 decodeModified :: [Encode a] -> [a]
 decodeModified = concatMap go
-	where go (Single a)     = [a]
-	      go (Multiple c a) = replicate c a
-
-testFunc :: a -> a
-testFun n = square
-	where lel = n*n
-	      square = lel
+    where go (Single a)     = [a]
+          go (Multiple c a) = replicate c a
