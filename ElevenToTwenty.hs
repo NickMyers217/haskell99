16fe673f755defae07fc656072440c5c52375165
nickmyers217 <nickbmyers217@gmail.com>
2015-07-14 07:01:45 -0500

    Fixed the indentation problems and finished up to 21

--- ElevenToTwenty.hs
+++ ElevenToTwenty.hs
@@ -6,14 +6,91 @@ import OneToTen(encode)
 data Encode a = Multiple Int a
               | Single a deriving (Show)
 
-
 encodeModified :: Eq a => [a] -> [Encode a]
 encodeModified = map go . encode
     where go (c,a) = if c == 1 then Single a else Multiple c a
-
 
 -- | Problem 12
 decodeModified :: [Encode a] -> [a]
 decodeModified = concatMap go
     where go (Single a)     = [a]
           go (Multiple c a) = replicate c a
+
+-- | Problem 13
+encodeDirect :: Eq a => [a] -> [Encode a]
+encodeDirect []  = []
+encodeDirect lst@(x:xs)
+    | dupes == 1 = Single         x : encodeDirect xs
+    | otherwise  = Multiple dupes x : encodeDirect rest
+        where dupes  = length $ takeWhile (== x) lst
+              rest   = dropWhile (== x) lst
+
+-- | Problem 14
+dupli :: [a] -> [a]
+dupli []     = []
+dupli (x:xs) = x : x : dupli xs
+
+-- | Problem 15
+repli :: [a] -> Int -> [a]
+repli []     _ = []
+repli xs     0 = xs
+repli xs     1 = xs
+repli (x:xs) n = replicate n x ++ repli xs n
+
+-- | Problem 16
+dropEvery :: [a] -> Int -> [a]
+dropEvery [] _     = []
+dropEvery _  0     = error "Enter 1 or greater."
+dropEvery _  1     = []
+dropEvery xs n     = (map fst . filter go) lst
+    where lst      = xs `zip` [1..length xs]
+          go (_,c) = c `mod` n /= 0
+
+-- | Problem 17
+split :: [a] -> Int -> ([a], [a])
+split []     _ = error "List is empty."
+split _      0 = error "Enter 1 or greater."
+split (x:xs) n = go [x] xs
+    where go front back@(y:ys)
+            | length front == n = (front, back)
+            | otherwise         = go (front ++ [y]) ys
+
+-- | Problem 18
+slice :: [a] -> Int -> Int -> [a]
+slice [] _ _ = error "List is empty."
+slice xs i k
+    | i == 0 || k == 0               = error "Invalid index."
+    | i > length xs || k > length xs = error "Out of bounds."
+    | otherwise                      = go xs
+        where go = take (k - i + 1) . drop (i - 1)
+
+-- | Problem 19
+rotate :: [a] -> Int -> [a]
+rotate [] _ = error "List is empty."
+rotate xs 0 = xs
+rotate xs n
+    | n > 0     = drop n xs ++ take n xs
+    | otherwise = front     ++ back
+        where front = drop (length xs - (-1 * n)) xs 
+              back  = take (length xs - (-1 * n)) xs
+
+-- | Problem 20
+removeAt :: Int -> [a] -> (a, [a])
+removeAt _ [] = error "List is empty."
+removeAt 0  _ = error "enter 1 or greater."
+removeAt n xs = (e, lst)
+    where e   = (last . take n) xs
+          lst = (init . take n) xs ++ drop n xs
+
+
+
+
+
+
+
+
+
+
+
+
+
--- OneToTen.hs
+++ OneToTen.hs
@@ -10,8 +10,8 @@ myLast (_:xs) = myLast xs
 myButLast :: [a] -> a
 myButLast []     = error "error"
 myButLast (x:xs) = if length xs == 1
-                   then x
-                   else myButLast xs
+                      then x
+                      else myButLast xs
 
 -- | Problem 3
 elementAt :: [a] -> Int -> a
@@ -19,8 +19,8 @@ elementAt [] _ = error "List is empty."
 elementAt _  0 = error "0 is not an element."
 elementAt xs 1 = head xs
 elementAt xs n
-	| n <= length xs = head . drop (n - 1) $ xs
-	| otherwise      = error "Not enough elements."
+    | n <= length xs = head . drop (n - 1) $ xs
+    | otherwise      = error "Not enough elements."
 
 -- | Problem 4
 myLength :: [a] -> Int
@@ -42,28 +42,26 @@ compress :: Eq a => [a] -> [a]
 compress []  = []
 compress [a] = [a]
 compress (x:xs)
-	| x == head xs = compress xs
-	| otherwise    = x : compress xs
+    | x == head xs = compress xs
+    | otherwise    = x : compress xs
 
 -- | Problem 9
 pack :: Eq a => [a] -> [[a]]
 pack = init . go
-	where
-	 go []  = [[]]
-	 go lst@(x:xs)
-		| null dupes  = [x]   : go xs
- 		| otherwise   = dupes : go rest
-	 		where 
-	 			dupes = takeWhile (== x) lst
-	 			rest  = dropWhile (== x) lst
+    where go []  = [[]]
+          go lst@(x:xs)
+            | null dupes  = [x]   : go xs
+            | otherwise   = dupes : go rest
+                where dupes = takeWhile (== x) lst
+                      rest  = dropWhile (== x) lst
 
 -- | Problem 10
 encode :: Eq a => [a] -> [(Int, a)]
 encode [] = []
 encode xs = zip lens elems
-	where lst   = pack xs
-	      elems = map head lst
-	      lens  = map length lst
+    where lst   = pack xs
+          elems = map head lst
+          lens  = map length lst
 
 
 
